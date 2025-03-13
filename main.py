1f5fdc18e2e090ffeee2d57ec056ea1ec9df26ca
lucaruboni <lucaruboni@gmail.com>
2025-03-13 23:23:45 +0100

    [fix] pos-env in pos_env

---
+++ main.py
@@ -0,0 +1,28 @@
+from kaki.app import App
+from kivy.factory import Factory
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+
+class MDLive(App, MDApp):
+    CLASSES = {
+        "POSPizzeriaApp": "pos-env.pos"
+    }
+
+
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+
+    def build_app(self, **kwargs):
+        print("Building app automatically")
+        # Crea l'istanza del widget radice tramite Factory
+        app_root = Factory.POSPizzeriaApp()
+        # Avvolgi in un contenitore (BoxLayout, FloatLayout, ecc.) per essere sicuri che abbia le proprietà necessarie
+        container = BoxLayout()
+        container.add_widget(app_root)
+        return container
+    
+
+
+if __name__ == "__main__": 
+    MDLive().run()
